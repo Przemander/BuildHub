6f80a2dc7ca1cb87eed6358734adaa6e99142aa4
Przemysław Jackowski <156017743+Przemander@users.noreply.github.com>
2025-10-26 13:48:46 +0100

    Delete frontend directory

--- frontend/src/services/authService.js
+++
@@ -1,9 +0,0 @@
-import apiClient from './apiClient';
-
-// Service for authentication-related API calls
-export default {
-  // Register function
-  register(username, email, password) {
-    return apiClient.post('/auth/register', { username, email, password });
-  }
-};
